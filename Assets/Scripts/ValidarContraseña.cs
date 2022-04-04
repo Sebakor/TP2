c008d92b53ce2acaabef4f15f78dbfc350e56d82
Sebakor <47070879@est.ort.edu.ar>
2022-04-04 11:41:04 -0300

    New cripts
    Create a new´s cripts, Mayor de edad and Validar Contraseña

---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contraUser;
+    public Text ingresoUser;
+    public Text textoMensaje;
+    public GameObject cartelMensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+    }
+
+    public void validarContraseña()
+    {
+        contraUser = ingresoUser.text;
+        if (contraUser == contraseñaCorrecta)
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelMensaje.SetActive(true);
+            textoMensaje.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
